eb63cb1668ad71506ddc799ecbf0e30b8ec9abcc
Rusomirt <rusomirt@gmail.com>
2017-03-09 19:47:27 +0600

    index: carousel responsive height (using jQuery) - remove JS console logging

--- js/custom.js
+++ js/custom.js
@@ -7,16 +7,9 @@ $(document).ready(function () {
 
     let currentScreenWidth = $(".carousel .carousel-item.active").width();
 
-    console.log("---------- RESIZE ----------");
-    console.log("currentScreenWidth = " + currentScreenWidth);
-
     $(".carousel .carousel-item").each( function(index) {
       $(this).height( currentScreenWidth/carouselImgRatio );
-      console.log("height(" + index + ") = " + $(this).height());
     });
-
-    // $(".carousel").css("min-height", current.height);
-    // $(".carousel-inner").css("height", current.height);
   }
 
   carouselOnResize();
