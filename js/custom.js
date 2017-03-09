9ec6c209d81618239daa751fcf375c5ed8cb4798
Rusomirt <rusomirt@gmail.com>
2017-03-09 19:42:39 +0600

    index: carousel responsive height (using jQuery)

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,28 @@
-/**
- * Created by Rusomirt on 07.03.2017.
- */
+$(document).ready(function () {
+
+  function carouselOnResize() {
+    let carouselImgWidth = 1600;
+    let carouselImgHeight = 500;
+    let carouselImgRatio = carouselImgWidth/carouselImgHeight;
+
+    let currentScreenWidth = $(".carousel .carousel-item.active").width();
+
+    console.log("---------- RESIZE ----------");
+    console.log("currentScreenWidth = " + currentScreenWidth);
+
+    $(".carousel .carousel-item").each( function(index) {
+      $(this).height( currentScreenWidth/carouselImgRatio );
+      console.log("height(" + index + ") = " + $(this).height());
+    });
+
+    // $(".carousel").css("min-height", current.height);
+    // $(".carousel-inner").css("height", current.height);
+  }
+
+  carouselOnResize();
+
+  $(window).resize( function() {
+    carouselOnResize();
+  });
+
+});
